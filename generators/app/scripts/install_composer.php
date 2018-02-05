0015440461b95b966548149e0a7698bf7fadda8d
Tobias Dühr <nerd@tobiasduehr.com>
2018-02-05 22:10:11 +0100

    install composer, add preview server to dploy.ini

---
+++ generators/app/scripts/install_composer.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "Installing Composer.phar" . PHP_EOL . PHP_EOL;
+copy('https://getcomposer.org/installer', 'composer-setup.php');
+if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') {
+    echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php');
+} echo PHP_EOL;
+echo `php composer-setup.php`;
+unlink('composer-setup.php');
