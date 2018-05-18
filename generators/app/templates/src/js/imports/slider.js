c15c0bea457380584d8adea09d3980a6b5fde643
Tobias Duehr <tobias@dieschittigs.de>
2018-05-18 21:36:53 +0200

    Update slider.js

--- generators/app/templates/src/js/imports/slider.js
+++ generators/app/templates/src/js/imports/slider.js
@@ -1,4 +1,34 @@
 import $ from 'jquery';
 import slick from 'slick-carousel';
 
-$(".slider").slick({});
+$(".content-slider").each(function(i){
+    const $el = $(this);
+    const opts = {
+        autoplay: false,
+        autoplaySpeed: 5000,
+        infinite: true,
+        speed: 300,
+        dots: true,
+        slidesToShow: 1
+    };
+    const config = $el.data('config').split(',');
+    config.forEach((val, index) => {
+        val = parseInt(val);
+        switch(index){
+            case 0:
+                opts.autoplaySpeed = val;
+                opts.autoplay = val > 0;
+            break;
+            case 1:
+                opts.speed = val;
+            break;
+            case 2:
+                opts.slidesToShow = val;
+            break;
+            case 3:
+                opts.infinite = val > 0;
+            break;
+        }
+    });
+    $el.children('.slider-wrapper').slick(opts);
+});
